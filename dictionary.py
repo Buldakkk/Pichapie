bb80dec6506f6b77aa3a7efc590e4a1ebdbc5ddd
Buldakkk <reyesqueencyruth@gmail.com>
2025-06-22 17:45:54 +0800

    mod

--- dictionary.py
+++ dictionary.py
@@ -6,3 +6,13 @@ companies = {"ECC": "Zamora St., Tarlac City, Tarlac",
              "Wireless Access for Health": "Diwa ng Tarlac"}
 
 print(companies.get("ECC"))  # this accessed the value of the key ECC
+print(companies.get("AYO"))  # since it is not in the list, it printed "None"
+
+
+ask = input("Enter a Company: ")
+
+if ask in companies:
+    # {companies[ask]} will print the VALUE of the entered KEY
+    print(f"It exists and it is located at {companies[ask]}")
+else:
+    print("ah wala")
