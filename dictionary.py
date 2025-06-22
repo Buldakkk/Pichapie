042ed3cc7fcf6ebdbf6d6c68c9fad914f2e3fe57
Buldakkk <reyesqueencyruth@gmail.com>
2025-06-22 17:30:57 +0800

    with contents

--- dictionary.py
+++ dictionary.py
@@ -1,1 +1,8 @@
 # DICTIONARY  is a collection of {key:value} pairs, ordered and changeable. No dupes
+
+companies = {"ECC": "Zamora St., Tarlac City, Tarlac",
+             "Civil Service Commission": "Tarlac City",
+             "Tarlac City Water District": " Maliwalo Tarlac City",
+             "Wireless Access for Health": "Diwa ng Tarlac"}
+
+print(companies.get("ECC"))  # this accessed the value of the key ECC
