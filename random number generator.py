10807dbf2bb4ba4797ecdfeaef0537a738d0a2ef
Buldakkk <reyesqueencyruth@gmail.com>
2025-06-22 19:05:18 +0800

    everyday we shuffelen

--- random number generator.py
+++ random number generator.py
@@ -13,3 +13,17 @@ print(randomizer)
 
 num = random.random()  # in the random module, we used random method
 print(num)  # this will print randum number between 0 and 1
+
+
+# Let's use the module to pick random strings
+
+game = ("rock", "paper", "scissors")
+picker = random.choice(game)
+print(picker)
+
+
+# Let's try to shuffle V's name
+name = ["V", "I", "C", "T", "O", "R", "I", "A"]
+
+random.shuffle(name)
+print(name)
