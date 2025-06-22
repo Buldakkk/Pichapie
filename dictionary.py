796e2b101828139b2ee20e0b2d19937f7489ae94
Buldakkk <reyesqueencyruth@gmail.com>
2025-06-22 18:05:24 +0800

    ril

--- dictionary.py
+++ dictionary.py
@@ -8,11 +8,19 @@ companies = {"ECC": "Zamora St., Tarlac City, Tarlac",
 print(companies.get("ECC"))  # this accessed the value of the key ECC
 print(companies.get("AYO"))  # since it is not in the list, it printed "None"
 
-
+'''
 ask = input("Enter a Company: ")
 
 if ask in companies:
     # {companies[ask]} will print the VALUE of the entered KEY
     print(f"It exists and it is located at {companies[ask]}")
 else:
-    print("ah wala")
+    print("ah wala")   
+'''
+
+companies.update({"LGU Capas": "Sto. Domingo 2nd, Capas, Tarlac"})
+print(companies)
+
+companies.pop("ECC")  # removes specific key:value
+companies.popitem()  # removes latest key:value
+print(companies)
